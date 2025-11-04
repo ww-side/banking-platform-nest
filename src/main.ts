86c615b433077b59b8a28a6cf76fef48b04e84df
ww-side <daniil.metelia@gmail.com>
2025-11-04 20:26:50 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,12 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 void bootstrap();
